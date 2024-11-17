094e9154a718b3514c51cea7af5f90b8fa541c03
MoPaMo <mopamo@users.noreply.github.com>
2024-11-17 10:44:11 +0100

    Add static files and update Next.js configuration for export

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+    distDir: 'dist',
+    output: 'export'
+
 };
+
 
 export default nextConfig;
